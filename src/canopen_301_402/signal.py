53b89ee3d652db5bd0a0f04be8a297b4648d1bc6
xaedes <xaedes@gmail.com>
2016-06-16 17:28:14 +0200

    try to fix issue with not called callback

--- src/canopen_301_402/signal.py
+++ src/canopen_301_402/signal.py
@@ -18,8 +18,14 @@ class Signal(object):
         if callback in self.callbacks_once: self.callbacks_once.remove(callback)
 
     def dispatch(self, *args, **kwargs):
+        callbacks = list()
         for callback in self.callbacks:
+            callbacks.append(callback)
+
+        while len(self.callbacks_once) > 0:
+            callback = self.callbacks_once.pop()
+            callbacks.append(callback)
+        
+        for callback in callbacks:
             callback(*args, **kwargs)
-        for callback in self.callbacks_once:
-            callback(*args, **kwargs)
-        self.callbacks_once = list()
+
