b8103231ed36898e20f2f479c01947283369cb3b
xaedes <xaedes@gmail.com>
2016-06-17 19:02:30 +0200

    add brave_new_world package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['canopen_301_402'],
+    packages=['canopen_301_402',"brave_new_world"],
     package_dir={'': 'src'},
     ##  don't do this unless you want a globally visible script
     # scripts=['bin/myscript'],
