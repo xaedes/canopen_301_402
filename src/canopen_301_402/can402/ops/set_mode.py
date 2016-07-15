ded6dd6920dbb942ba57f66b2a0aff4c487ec031
xaedes <xaedes@gmail.com>
2016-07-15 21:18:30 +0200

    use SdoWriteObject instead of SdoWrite

--- src/canopen_301_402/can402/ops/set_mode.py
+++ src/canopen_301_402/can402/ops/set_mode.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from canopen_301_402.constants import *
-from canopen_301_402.async.sdo_write import SdoWrite
+from canopen_301_402.async.sdo_write_object import SdoWriteObject
 
-class SetMode(SdoWrite):
+class SetMode(SdoWriteObject):
     """docstring for SetMode"""
     def __init__(self, node, mode, *args, **kwargs):
+    	# print mode.value
+    	parameter_name = "Modes of Operation"
+        value = mode.value
+        # index, subindex = Can402Objects.modes_of_operation_set
 
-        data = [Can402ModesOfOperationBits[mode]]
-        index, subindex = Can402Objects.modes_of_operation_set
-
-        super(SetMode, self).__init__(node, index, subindex, data, *args, **kwargs)
+        super(SetMode, self).__init__(node, parameter_name, value, *args, **kwargs)
         
