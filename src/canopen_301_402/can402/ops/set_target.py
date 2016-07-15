7a1a78fec9bba937a550468d95ae975b53916d8c
xaedes <xaedes@gmail.com>
2016-07-15 21:18:53 +0200

    use SdoWriteObject

--- src/canopen_301_402/can402/ops/set_target.py
+++ src/canopen_301_402/can402/ops/set_target.py
@@ -5,7 +5,7 @@ from funcy import partial
 
 from canopen_301_402.constants import *
 from canopen_301_402.async.async_chain import AsyncChain
-from canopen_301_402.async.sdo_write import SdoWrite
+from canopen_301_402.async.sdo_write_object import SdoWriteObject
 from canopen_301_402.can402.ops.notify_new_target import NotifyNewTarget
 
 import struct
@@ -13,17 +13,17 @@ import struct
 
 class SetTarget(AsyncChain):
     """docstring for SetTarget"""
-    def __init__(self, node, value, relative=False, immediatly=False, *args, **kwargs):
+    def __init__(self, node, value, relative=False, immediatly=False, target_type="Position", *args, **kwargs):
+        parameter_name = "Target " + target_type
         self.node = node
         self.value = value
         self.relative = relative
         self.immediatly = immediatly
 
-        set_target_position = partial(SdoWrite,
+        set_target = partial(SdoWriteObject,
                                 node = node, 
-                                index = Can402Objects.target_position[0], 
-                                subindex = Can402Objects.target_position[1], 
-                                data = struct.pack("<i",self.value))
+                                parameter_name = parameter_name, 
+                                value = self.value)
         notify_new_target   = partial(NotifyNewTarget,
                                 node = node, 
                                 relative = self.relative, 
@@ -31,7 +31,7 @@ class SetTarget(AsyncChain):
 
 
 
-        operations = [set_target_position, notify_new_target]
+        operations = [set_target, notify_new_target]
 
         super(SetTarget, self).__init__(node, operations, *args, **kwargs)
         
